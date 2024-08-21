3eaa1bb6a750bae495c4959b74b3dc97bffd31bd
SongMinJe <dlsgur3543@gmail.com>
2024-08-21 17:27:05 +0900

    test

---
+++ Assets/02_Scripts/ItemData/ItemBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    public string uuid;
+    public string itemName;
+    public string date;
+    public bool special;
+}
--- Assets/KKM/ItemManager.cs
+++ Assets/KKM/ItemManager.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    public List<string> collectedItems = new List<string>(); // 획득한 아이템 리스트
+    public List<ItemBehavior> collectedItems = new List<ItemBehavior>(); // 획득한 아이템 리스트
 
     // 아이템을 리스트에 등록하는 메서드
-    public void AddItem(string itemName)
+    public void AddItem(ItemBehavior itemName)
     {
         collectedItems.Add(itemName);
         Debug.Log(itemName + " has been added to the inventory.");
